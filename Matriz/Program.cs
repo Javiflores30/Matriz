4dcebd1a4551287da02b8367693b8b10a773d5d4
Javiflores30 <109160991+Javiflores30@users.noreply.github.com>
2022-10-13 19:41:36 -0600

    Add files via upload

---
+++ Matriz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+		int[,] matriz = new int[10,10];
+		
+		for (int filas = 1; filas< matriz.GetLength(0); filas++)
+		{
+			Console.WriteLine("");
+			for (int col = 1; col < matriz.GetLength(1); col++)
+			{
+				matriz[filas,col]  = filas*col;
+				Console.Write(matriz[filas,col]);
+	
+				Console.Write("  ");
+			}
+			
+		}
+//------------------------------
+	}
+}
